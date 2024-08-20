c4c08e9af06872d706ba96b7309f5f904af5d1a0
Daniela Pinheiro <daniela.plp@gmail.com>
2024-08-21 00:17:44 +0100

    terça

--- src/pages/CycleRoutes.jsx
+++ src/pages/CycleRoutes.jsx
@@ -1,94 +1,80 @@
-import React from 'react'
-import { Link, useNavigate } from "react-router-dom"
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
-import axios from 'axios'
-import { AuthContext } from '../contexts/auth.context';
-import { GoogleMap, LoadScript, Marker, Polyline } from '@react-google-maps/api';
+import axios from "axios";
+import { AuthContext } from "../contexts/auth.context";
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  Polyline,
+} from "@react-google-maps/api";
 
-import CycleAPIService from '../services/cycle.api';
-
+import CycleAPIService from "../services/cycle.api";
 
 const cycleService = new CycleAPIService();
 
 const containerStyle = {
-  width: '100%',
-  height: '400px'
+  width: "100%",
+  height: "400px",
 };
 
 const center = {
   lat: -3.745,
-  lng: -38.523
+  lng: -38.523,
 };
 
 function CycleRoutes() {
-const [cycleRoutes, setCycleRoutes] = useState([]);
-const navigate = useNavigate();
-const {user} = useContext(AuthContext)
-
-
-
+  const [cycleRoutes, setCycleRoutes] = useState([]);
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
   const fetchData = async () => {
     try {
-       const response = await cycleService.getAllCycleRoutes()
-       setCycleRoutes(response.data) 
-       console.log(response.data)
+      const response = await cycleService.getAllCycleRoutes();
+      setCycleRoutes(response.data);
+      console.log(response.data);
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
+  };
 
-};
-
-
-
-useEffect(() => {
-console.log('useEffect: Mounting')
-fetchData();
-},  []);
+  useEffect(() => {
+    console.log("useEffect: Mounting");
+    fetchData();
+  }, []);
 
   return (
     <div>
-      
       <h1>Cycle Routes</h1>
-           
-            {cycleRoutes.map(cycleroute => {
-                return (
-                    <div key={cycleroute._id}>
-                        <Link to={`/cycleroutes/${cycleroute._id}`}>
-                        <h2>{cycleroute.type}</h2>
-                        </Link>
-                        <p>{cycleroute.startLocation?.lat || 'No Lat'}</p>
-                        <p>{cycleroute.startLocation?.lng || 'No Lng'}</p>
-                        <p>{cycleroute.endLocation?.lat || 'No Lat'}</p>
-                        <p>{cycleroute.endLocation?.lng || 'No Lng'}</p>
-                        
-      <LoadScript
-      googleMapsApiKey="AIzaSyAmQDQ_YE5BHV1EFMu717cncucLvLL93ic"
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-       
-        zoom={12}>
 
-       {/*  <Marker position={cycleroute.startLocation?.lat} label="Início" />
+      {cycleRoutes.map(cycleroute => {
+        return (
+          <div key={cycleroute._id}>
+            <Link to={`/cycleroutes/${cycleroute._id}`}>
+              <h2>{cycleroute.type}</h2>
+            </Link>
+            <p>{cycleroute.startLocation?.lat || "No Lat"}</p>
+            <p>{cycleroute.startLocation?.lng || "No Lng"}</p>
+            <p>{cycleroute.endLocation?.lat || "No Lat"}</p>
+            <p>{cycleroute.endLocation?.lng || "No Lng"}</p>
+
+            <LoadScript googleMapsApiKey>
+              <GoogleMap
+                mapContainerStyle={containerStyle}
+                zoom={12}>
+                {/*  <Marker position={cycleroute.startLocation?.lat} label="Início" />
                 <Marker position={cycleroute.endLocation?.lat && cycleroute.endLocation?.lng } label="Fim" />
                 <Polyline  options={{ strokeColor: "#FF0000", strokeWeight: 2 }} /> */}
-       
-        <></>
-      </GoogleMap>
-    </LoadScript>
-                        
-                        </div>
-                );
-                
-        
-            })}
-            
-            
-      
-      
-      </div>
-  )
+
+                <></>
+              </GoogleMap>
+            </LoadScript>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default CycleRoutes
+export default CycleRoutes;
